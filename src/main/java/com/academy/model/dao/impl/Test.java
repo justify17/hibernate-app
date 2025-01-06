3637124360a15676fbc0816ffb00b298cc2a20cb
Roma <roma.goncharov.1997@mail.ru>
2025-01-06 20:32:31 +0300

    delete test

--- src/main/java/com/academy/model/dao/impl/Test.java
+++
@@ -1,8 +0,0 @@
-package com.academy.model.dao.impl;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("TEST");
-        System.out.println("DONE");
-    }
-}
